7b51063a9a7c0a534466ccccef21f81a1430b6bf
Chris <orthagonal1@gmail.com>
2016-12-12 18:31:40 -0600

    add comments

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const path = require('path');
 class AssetMap {
 
   constructor(options) {
+    // can pass in an object ot use as the asset map:
     this.assetMap = typeof options.assetMap === 'object' ? options.assetMap : false;
+    // otherwise will read the asset map from file:
     this.pathToAssetMap = options.pathToAssetMap;
   }
 
+  // looks things up in the assetMap when it exists:
   lookupInMap(fileName) {
     if (this.assetMap[fileName]) {
       return this.assetMap[fileName];
@@ -18,23 +21,25 @@ class AssetMap {
     }
   }
 
-  handleMap(originalName, mappedName, done) {
+  // calls the callback with error if it didn't work:
+  handleCallback(originalName, mappedName, done) {
     if (mappedName) {
       return done(null, mappedName);
     }
     return done(new Error(`Could not find ${originalName} in assetMap`));
   }
 
+  // public facing function that looks in the map or loads it from file if not found:
   lookupAsset(fileName, done) {
     if (this.assetMap) {
-      this.handleMap(fileName, this.lookupInMap(fileName), done);
+      this.handleCallback(fileName, this.lookupInMap(fileName), done);
     }
     fs.readFile(this.pathToAssetMap, (err, data) => {
       if (err) {
         return done(err);
       }
       this.assetMap = JSON.parse(data.toString());
-      this.handleMap(fileName, this.lookupInMap(fileName), done);
+      this.handleCallback(fileName, this.lookupInMap(fileName), done);
     });
   }
 }
